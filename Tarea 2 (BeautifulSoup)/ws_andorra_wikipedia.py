52d1a2504324f6319897b40318e906ac98e53684
DoramasRodriguez <doramasrodriguez2004@gmail.com>
2025-03-24 12:23:23 +0100

    Web Scraping con Beatiful Soup

--- Tarea 2 (BeautifulSoup)/ws_andorra_wikipedia.py
+++ Tarea 2 (BeautifulSoup)/ws_andorra_wikipedia.py
@@ -1,7 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+import re
 
 url = "https://es.wikipedia.org/wiki/Andorra"
+table_class = 'infobox geography vcard'
 
 respuesta = requests.get(url)
 soup = BeautifulSoup(respuesta.text, "html.parser")
@@ -9,8 +12,9 @@ soup = BeautifulSoup(respuesta.text, "html.parser")
 parrafos = soup.find_all('p')
 primer_parrafo = parrafos[1]
 
-tds = soup.find_all('td')
-datopoblacion = tds[21]
+tds = soup.find_all(text=re.compile(".*km²"), limit=4)
 
 print(primer_parrafo.text)
-print(datopoblacion.text)
+
+for td in tds:
+    print(td.strip())

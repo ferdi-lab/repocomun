aa0361d489fc45f2a1a1e58358469cd3248e8d87
DoramasRodriguez <doramasrodriguez2004@gmail.com>
2025-03-31 11:37:40 +0200

    - Hilos - 7 horas - Creé un hilo para buscar oposiciones en el Boletín Estatal y otro para el Boletín Autonómico, al final del programa se borran los PDF.

--- Tarea 6 (BOE)/lector_pdf.py
+++ Tarea 6 (BOE)/lector_pdf.py
@@ -1,18 +1,40 @@
-import fitz  # PyMuPDF
-import re  # Expresiones regulares
+import fitz
+import re
+import threading
+import os
 
-doc = fitz.open("D:\Escritorio\Repocomun\Tarea 6 (BOE)\oposiciones.pdf")
 
-# Expresión regular para buscar "jurista"
-pattern = re.compile(r'\bjurista\b', re.IGNORECASE)
+boletin_estatal = fitz.open(r"D:\Escritorio\Repocomun\Tarea 6 (BOE)\oposiciones.pdf")
+boletin_autonomico = fitz.open(r"D:\Escritorio\Repocomun\Tarea 6 (BOE)\oposiciones.pdf")
 
-# Buscar en cada página
-for page_num in range(len(doc)):
-    page = doc[page_num]
-    text = page.get_text("text")
+
+def buscar_en_documento(boletin, nombre):
+    pattern = re.compile(r'\bjurista\b', re.IGNORECASE)
     
-    # Buscar coincidencias con la expresión regular
-    matches = pattern.findall(text)
-    
-    if matches:
-        print(f"Encontrado en la página {page_num + 1}: {len(matches)} veces")
+    for page_num in range(len(boletin)):
+        page = boletin[page_num]
+        text = page.get_text("text")
+        
+        matches = pattern.findall(text)
+        if matches:
+            print(f"{nombre}: Encontrado en la página {page_num + 1}: {len(matches)} veces")
+
+
+# Crear hilos
+hilo1 = threading.Thread(target=buscar_en_documento, args=(boletin_estatal, "Boletín Estatal"))
+hilo2 = threading.Thread(target=buscar_en_documento, args=(boletin_autonomico, "Boletín Autonómico"))
+
+
+hilo1.start()
+hilo1.join()
+
+hilo2.start()
+hilo2.join()
+
+# Cerrar y eliminar los archivos
+boletin_estatal.close()
+boletin_autonomico.close()
+
+os.remove("D:\Escritorio\Repocomun\Tarea 6 (BOE)\oposiciones.pdf")
+print("Archivos PDF eliminados correctamente.")
+
